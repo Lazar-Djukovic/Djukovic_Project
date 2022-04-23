7008b12e37a30660888b09215c9a6653c8272bd7
Lazar-Djukovic <ldjukovic40@gmail.com>
2022-04-23 12:52:08 +0100

    Basic mechanics for the game
    The camera works, but i need to work more on the mouse tracking for the bullet angle.

--- Lazar's Shooter/LazarsShooter.py
+++ Lazar's Shooter/LazarsShooter.py
@@ -1,50 +1,98 @@
 import pygame
- 
-# Define some colors
-BLACK = (0, 0, 0)
-WHITE = (255, 255, 255)
-GREEN = (0, 255, 0)
-RED = (255, 0, 0)
- 
+import math
+
+
 pygame.init()
- 
-# Set the width and height of the screen [width, height]
-size = (700, 500)
-screen = pygame.display.set_mode(size)
- 
-pygame.display.set_caption("My Game")
- 
-# Loop until the user clicks the close button.
-done = False
- 
-# Used to manage how fast the screen updates
+
+GREEN = (43,132,88)
+RED = (255,0,0)
+WHITE = (255,255,255)
+BLACK = (0,0,0)
+
+SCREEN_W = 1280
+SCREEN_H = 720
+display = pygame.display.set_mode((SCREEN_W,SCREEN_H))
+pygame.display.set_caption('/> Shooter')
 clock = pygame.time.Clock()
- 
-# -------- Main Program Loop -----------
-while not done:
-    # --- Main event loop
+
+class Player:
+  def __init__(self, x, y, width, height):
+    self.x = x
+    self.y =y
+    self.width = width
+    self.height = height
+  
+  def CameraUpdate(self,display):
+    pygame.draw.rect(display, RED, (self.x, self.y, self.width, self.height))
+
+
+class PlayerBullet:
+  def __init__(self, x, y, mouse_x, mouse_y):
+    self.x = x
+    self.y = y
+    self.mouse_x = mouse_x
+    self.mouse_y = mouse_y
+
+    self.speed = 15
+    
+    self.angle = math.atan2(self.mouse_y, self.mouse_x)
+    self.x_vel = math.cos(self.angle) * self.speed
+    self.y_vel = math.sin(self.angle) * self.speed
+
+  def update(self,display):
+    self.x -= int(self.x_vel)
+    self.y -= int(self.y_vel)
+
+    pygame.draw.circle(display, (BLACK), (self.x, self.y),5)
+
+
+player = Player(640,360,32,32)
+
+display_scroll = [0,0]
+
+player_bullets = []
+
+
+def gameLoop():
+  gameExit = False
+  gameOver = False
+
+  while not gameExit:
+    display.fill(GREEN)
+
+    mouse_x, mouse_y = pygame.mouse.get_pos()
+
     for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            done = True
- 
-    # --- Game logic should go here
- 
-    # --- Screen-clearing code goes here
- 
-    # Here, we clear the screen to white. Don't put other drawing commands
-    # above this, or they will be erased with this command.
- 
-    # If you want a background image, replace this clear with blit'ing the
-    # background image.
-    screen.fill(WHITE)
- 
-    # --- Drawing code should go here
- 
-    # --- Go ahead and update the screen with what we've drawn.
-    pygame.display.flip()
- 
-    # --- Limit to 60 frames per second
+      if event.type == pygame.QUIT:
+        pygame.quit()
+
+      if event.type == pygame.MOUSEBUTTONDOWN:
+        if event.button == 1:
+          player_bullets.append(PlayerBullet(player.x, player.y, mouse_x, mouse_y))
+
+    pygame.draw.rect(display, WHITE, (100-display_scroll[0],100-display_scroll[1],16,16))
+
+    keys = pygame.key.get_pressed()
+
+    if keys[pygame.K_a]:
+      display_scroll[0] -= 5
+    if keys[pygame.K_d]:
+      display_scroll[0] += 5
+    if keys[pygame.K_w]:
+      display_scroll[1] -= 5
+    if keys[pygame.K_s]:
+      display_scroll[1] += 5
+  
+
+    player.CameraUpdate(display)
+
+    for bullet in player_bullets:
+      bullet.update(display)
+
+
+
     clock.tick(60)
- 
-# Close the window and quit.
-pygame.quit()
+    pygame.display.update()
+
+  
+gameLoop()
